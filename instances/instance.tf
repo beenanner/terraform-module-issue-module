935f31455327e9b90fa35916485ed7a5c546c16b
Jonathan Lee <Jonathan.Lee@cbsinteractive.com>
2017-07-24 11:56:35 -0400

    init instance module

---
+++ instances/instance.tf
@@ -0,0 +1,44 @@
+variable "name" {}
+
+variable "zones" {
+  type = "list"
+}
+
+variable "instance_count" {}
+
+variable "image" {}
+
+variable "subnetwork" {}
+
+variable "subnetwork_project" {}
+
+resource "google_compute_instance" "instance" {
+  name         = "${var.name}-instance-${count.index+1}"
+  count        = "${var.instance_count}"
+  machine_type = ""
+  zone         = "${element(var.zones, count.index)}"
+
+  provisioner "remote-exec" {
+    connection {
+      host = "${self.network_interface.0.address}"
+      user = "ubuntu"
+    }
+  }
+
+  disk {
+    image = "${var.image}"
+  }
+
+  network_interface {
+    subnetwork = "${var.subnetwork}"
+    subnetwork_project = "${var.subnetwork_project}"
+
+    access_config {
+      # ephemeral
+    }
+  }
+}
+
+output "instance_private_ips" {
+  value = ["${google_compute_instance.instance.*.network_interface.0.address}"]
+}
